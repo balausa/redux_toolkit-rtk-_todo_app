43bcb87eb8ba96488b36daebc80200a8817770bd
balausa <52880784+balausa@users.noreply.github.com>
2022-12-14 15:42:10 +0600

    start

---
+++ src/components/TodoItem.js
@@ -0,0 +1,16 @@
+
+const TodoItem = ({id, text, completed,toggleTodoComplete,removeTodo}) => {
+  return (
+    <li>
+    <input 
+    type="checkbox" 
+    checked={completed} 
+    onChange={()=>toggleTodoComplete(id)}/>
+    <span>{text}</span>
+    <span className='delete' onClick={()=> removeTodo(id)}>&times;
+    </span>
+    </li>
+  )
+}
+
+export default TodoItem
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import TodoItem from './TodoItem'
+
+const TodoList = ({ todos, toggleTodoComplete, removeTodo }) => {
+    return (
+        <ul>
+            {
+                todos.map(todo => <TodoItem 
+                    key={todo.id} 
+                    toggleTodoComplete={toggleTodoComplete}
+                    removeTodo={removeTodo}
+                    {...todo} />)
+            }
+        </ul>
+    )
+}
+
+export default TodoList
